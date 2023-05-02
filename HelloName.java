c4396b9e33d9ea8209083f19815349c05e21e47e
Seremba Patrick <pserembae@gmail.com>
2023-05-02 16:55:10 +0300

    HelloName challenge

---
+++ HelloName.java
@@ -0,0 +1,14 @@
+public class HelloName{
+  
+  public static void main(String[] args){
+    //https://codingbat.com/prob/p171896
+    String ans = helloName("Bob");
+    System.out.println(ans);
+  }
+  
+  public static String helloName(String name) {
+   
+  return "Hello " + name + "!";
+}
+
+}
