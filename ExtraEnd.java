1f0d2b41a35a970d2d912ee4b0d3d77d23777e28
Seremba Patrick <pserembae@gmail.com>
2023-05-03 13:42:11 +0300

    ExtraEnd challenge

---
+++ ExtraEnd.java
@@ -0,0 +1,19 @@
+public class ExtraEnd{
+
+   public static void main(String[] args){
+       
+       String ans = extraEnd("Hello");
+       
+       System.out.println(ans);
+      
+   }
+   
+   public static String extraEnd(String str) {
+   
+   String s = "";
+   for(int i = 0; i <=2; i++) {
+     s = s + str.charAt(str.length()-2) + str.charAt(str.length()-1);
+   }
+   return s;
+}
+}
