51f478fb20b638b96ba27712bcd987806b962f09
Seremba Patrick <pserembae@gmail.com>
2023-05-10 23:22:22 +0300

    makeApi challenge

---
+++ array-1/makePi.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+public class makePi{
+   public static void main(String[] args){
+      // https://codingbat.com/prob/p167011
+      int[] ans = makePi();
+      System.out.println(Arrays.toString(ans));
+   }
+   
+   public static int[] makePi() {
+      int[] pie = {3, 1, 4};
+      return pie;
+   }
+}
