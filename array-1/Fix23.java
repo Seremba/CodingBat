634cc836fa7fbb40448b6e39f4f036f8c179e5dc
Seremba Patrick <pserembae@gmail.com>
2023-05-11 19:49:39 +0300

    Fix23 challenge

---
+++ array-1/Fix23.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Fix23{
+   public static void main(String[] args){
+     // https://codingbat.com/prob/p120347
+      int[] nums = {1, 2, 3};
+      int[] ans = fix23(nums);
+      System.out.println(Arrays.toString(ans));
+   }
+   
+   public static int[] fix23(int[] nums) {
+     if(nums[0] == 2 && nums[1] == 3) {
+        nums[1] = 0;
+     }
+  
+    if(nums[1] == 2 && nums[nums.length-1] == 3){
+       nums[nums.length-1] = 0;
+    }
+  
+    return new int[]{nums[0],nums[1], nums[2]};
+  }
+
+}
