0a3073903cdf4eea33609906e2be2d3abd608422
Seremba Patrick <pserembae@gmail.com>
2023-05-11 10:52:37 +0300

    reverse3 challenge

--- array-1/Reverse3.java
+++ array-1/Reverse3.java
@@ -1,19 +1,19 @@
 import java.util.Arrays;
 
-public class RotateLeft3{
+public class Reverse3{
    public static void main(String[] args){
-     // https://codingbat.com/prob/p185139
+     // https://codingbat.com/prob/p112409
       int[] nums = {11, 9, 5};
-      int[] ans = rotateLeft3(nums);
+      int[] ans = reverse3(nums);
       System.out.println(Arrays.toString(ans));
    }
    
-   public static int[] rotateLeft3(int[] nums) {
-     int[] arr = new int[3];
-     arr[0] = nums[1];
-     arr[1] = nums[2];
-     arr[2] = nums[0];
+   public static int[] reverse3(int[] nums) {
   
+     int[] arr = new int[3];
+     arr[0] = nums[2];
+     arr[1] = nums[1];
+     arr[2] = nums[0];
     return arr;
 }
 
