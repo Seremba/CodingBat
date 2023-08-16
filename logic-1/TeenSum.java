4b1c4872ce18a8ad80fe074f38e50d96b2b0c4ea
Seremba Patrick <pserembae@gmail.com>
2023-08-17 00:41:27 +0300

    TeenSum challenge

---
+++ logic-1/TeenSum.java
@@ -0,0 +1,13 @@
+public class TeenSum {
+   public static void main(String[] args) {
+    // https://codingbat.com/prob/p178728
+   }
+
+   public static int teenSum(int a, int b) {
+    if(a >= 13 && a <= 19 || b >= 13 && b <= 19) {
+      return 19;
+   }
+    return a + b;
+  }
+
+}
