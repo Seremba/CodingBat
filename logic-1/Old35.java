657b3c01f7cc3309f06b54f79e6af39ebb5046c8
Seremba Patrick <pserembae@gmail.com>
2023-08-16 23:02:24 +0300

    Old35 challenge

---
+++ logic-1/Old35.java
@@ -0,0 +1,10 @@
+public class Old35 {
+   public static void main(String[] args) {
+    // https://codingbat.com/prob/p159612
+   }
+
+   public boolean old35(int n) {
+     return (n % 3 == 0) != (n % 5 == 0);
+  }
+
+}
