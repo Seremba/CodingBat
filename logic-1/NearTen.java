4a1aac255e0510d2a650ed3151f144fada38bf65
Seremba Patrick <pserembae@gmail.com>
2023-08-16 23:46:19 +0300

    NearTen challenge

---
+++ logic-1/NearTen.java
@@ -0,0 +1,10 @@
+public class NearTen {
+   public static void main(String[] args) {
+    // https://codingbat.com/prob/p193613
+   }
+
+   public static boolean nearTen(int num) {
+     return num % 10 <= 2 || num % 10 >= 8;
+  }
+
+}
