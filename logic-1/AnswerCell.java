60f22f19ae467b36645667eca4306830446b866b
Seremba Patrick <pserembae@gmail.com>
2023-08-17 00:58:59 +0300

    AnswerCell challenge

---
+++ logic-1/AnswerCell.java
@@ -0,0 +1,10 @@
+public class AnswerCell {
+   public static void main(String[] args) {
+    // https://codingbat.com/prob/p110973
+   }
+
+   public static answerCell(boolean isMorning, boolean isMom, boolean isAsleep) {
+     return (!isAsleep && !isMorning) || (!isAsleep && isMom);
+   }
+
+}
