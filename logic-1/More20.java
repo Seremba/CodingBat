29a0156311e4ef74a42bf33ccc3c6d52408956f1
Seremba Patrick <pserembae@gmail.com>
2023-08-16 22:49:11 +0300

    more20 challenge

---
+++ logic-1/More20.java
@@ -0,0 +1,13 @@
+public class More20 {
+   public static void main(String[] args) {
+    // https://codingbat.com/prob/p118290
+   }
+
+   public static boolean more20(int n) {
+  if (n % 20 == 1 || n % 20 == 2) {
+    return true;
+  }
+  return false;
+}
+
+}
