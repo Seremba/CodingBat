6e8bcf9c0be06fcc6c04f7c15915f44b429dea13
Seremba Patrick <45463621+Seremba@users.noreply.github.com>
2023-09-13 14:13:55 +0000

    fizzString challenge

--- logic-1/TeaParty.java
+++ logic-1/TeaParty.java
@@ -1,4 +1,5 @@
 public class TeaParty {
+    // https://codingbat.com/prob/p177181
     public int teaParty(int tea, int candy) {
        if(tea < 5 || candy < 5) return 0;
        if(tea >= 2*candy || candy >= 2*tea) return 2;
