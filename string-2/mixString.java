3b46b225b6c3791e4551d009e3cc760b79ab322d
Seremba Patrick <45463621+Seremba@users.noreply.github.com>
2023-09-27 08:54:14 +0000

    indent properly

--- string-2/mixString.java
+++ string-2/mixString.java
@@ -5,11 +5,11 @@ public String mixString(String a, String b) {
     
     for (int i = 0; i < max; i++) {
         if (i <= a.length() - 1) {
-        result += a.substring(i, i+1);
+            result += a.substring(i, i+1);
         }
         
         if (i <= b.length() - 1) {
-        result += b.substring(i, i+1);
+            result += b.substring(i, i+1);
         }
     }
     
