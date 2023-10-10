dcfd3366456680991461a390428424fed80e2c0c
Seremba Patrick <45463621+Seremba@users.noreply.github.com>
2023-10-10 13:15:41 +0000

    oneTwo challenge

---
+++ string-2/oneTwo.java
@@ -0,0 +1,10 @@
+public String oneTwo(String str) {
+  String output = "";
+  int length = str.length();
+  for (int i = 0; i < length - 2; i += 3) {
+       output += str.substring(i+1, i+3) + str.charAt(i);
+    
+  }
+  
+  return output;
+}
