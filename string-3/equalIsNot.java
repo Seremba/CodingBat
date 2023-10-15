6818ba64b6c69e98689ae36b5700ca465d076626
Seremba Patrick <45463621+Seremba@users.noreply.github.com>
2023-10-15 20:30:36 +0000

    equalIsNot challenge

---
+++ string-3/equalIsNot.java
@@ -0,0 +1,12 @@
+public boolean equalIsNot(String str) {
+  int is = 0;
+  int not = 0;
+  for (int i = 0; i < str.length(); i++) {
+    if (str.startsWith("is", i)) {
+      is++;
+    } else if (str.startsWith("not", i)) {
+      not++;
+    }
+  }
+  return is == not;
+}
