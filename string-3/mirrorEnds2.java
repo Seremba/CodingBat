5b1528075a9e19b4fd62a5bb69d1c0f3e8d8adc9
Seremba Patrick <45463621+Seremba@users.noreply.github.com>
2023-10-17 05:48:42 +0000

    mirrorEnds solution

--- string-3/mirrorEnds2.java
+++ string-3/mirrorEnds2.java
@@ -1,1 +1,12 @@
 // https://codingbat.com/prob/p139411
+public String mirrorEnds(String string) {
+  String output = "";
+  for (int i = 0; i < string.length(); i++) {
+    if (string.charAt(i) == string.charAt(string.length() - 1 - i)) {
+      output += string.charAt(i);
+    } else {
+      break;
+    }
+  }
+  return output;
+}
