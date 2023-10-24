cb8cd709989ea458d16d7e25309bc2b850bb7490
Seremba Patrick <45463621+Seremba@users.noreply.github.com>
2023-10-24 22:22:40 +0000

    add link to challenge

--- array-2/isEverywhere.java
+++ array-2/isEverywhere.java
@@ -1,4 +1,4 @@
-// 
+// https://codingbat.com/prob/p110222
 public boolean isEverywhere(int[] nums, int val) {
   for (int i = 0; i < nums.length - 1; i++) {
     if(nums[i] != val && nums[i+1] != val) {
