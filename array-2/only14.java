8bc8dd51b560835a6dfb10f55e9e88a53c7ff60c
Seremba Patrick <45463621+Seremba@users.noreply.github.com>
2023-10-24 22:15:55 +0000

    only14 challenge

---
+++ array-2/only14.java
@@ -0,0 +1,9 @@
+public boolean only14(int[] nums) {
+  boolean isOneOrFour = true;
+  for (int i = 0; i < nums.length; i++) {
+    if (nums[i] != 1 && nums[i] != 4) {
+      isOneOrFour = false;
+    }
+  }
+  return isOneOrFour;
+}
