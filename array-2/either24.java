e71aabe2327601cc590751c9f63d78cdef01439c
Seremba Patrick <45463621+Seremba@users.noreply.github.com>
2023-10-25 21:38:05 +0000

    either24 challenge

---
+++ array-2/either24.java
@@ -0,0 +1,16 @@
+// 
+public boolean either24(int[] nums) {
+  boolean twoNextTwo = false;
+  boolean fourNextFour = false;;
+  for (int i = 0; i < nums.length -1; i++) {
+    if (nums[i] == 2 && nums[i+1] == 2) {
+      twoNextTwo = true;
+    }
+    
+    if (nums[i] == 4 && nums[i+1] == 4) {
+      fourNextFour = true;
+    }
+    
+  }
+  return fourNextFour != twoNextTwo;
+}
